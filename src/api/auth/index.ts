2433aa8115b90ffe3f987054b015bfbacefac373
mksotto <maks.otto06@mail.ru>
2025-04-05 17:57:25 +0300

    register authGet and authDelete

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,12 +1,20 @@
 import {FastifyInstance, FastifyPluginCallback} from "fastify";
 import {authSignupPost} from "./authSignupPost";
 import {authLoginPost} from "./authLoginPost";
+import {authGet} from "./authGet";
+import {authDelete} from "./authDelete";
 
 export const authHandler: FastifyPluginCallback = (
     fastify: FastifyInstance,
     _,
     done,
 ) => {
+    // GET /auth
+    authGet(fastify);
+
+    // DELETE /auth
+    authDelete(fastify);
+
     // POST /auth/signup
     authSignupPost(fastify);
 
