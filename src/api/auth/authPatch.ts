db6906f0ad7fb9835511a250f8069cd2bdba9599
mksotto <maks.otto06@mail.ru>
2025-04-09 20:12:52 +0300

    username check on update

--- src/api/auth/authPatch.ts
+++ src/api/auth/authPatch.ts
@@ -5,6 +5,7 @@ import {AuthPatch} from "../../types/domain/todo-list-api";
 import {db} from "../../db";
 import bcrypt from "bcrypt";
 import {BadRequestError} from "../../errors/BadRequestError";
+import {ConflictError} from "../../errors/ConflictError";
 
 export const authPatch = (f: FastifyInstance) => {
     f.patch<{ Body: AuthPatch }>('/', withErrorHandler(withAuth(async (
@@ -15,6 +16,9 @@ export const authPatch = (f: FastifyInstance) => {
         const {username, password, new_password} = req.body;
         if (!await bcrypt.compare(password, user.password)) {
             throw new BadRequestError("Invalid password");
+        }
+        if (await db.users.findFirst({where: {username}})) {
+            throw new ConflictError("Username already exists!");
         }
         await db.users.update({
             where: {id: user.id},
